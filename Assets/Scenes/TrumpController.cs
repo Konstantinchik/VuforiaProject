6b96ed7df3a2e7a3be29b086ec8e7b8bd4add631
Konstantinchik <kgluschenko83@gmail.com>
2021-03-29 12:45:14 +0300

    VuforiaProject deploy
    AR Application

---
+++ Assets/Scenes/TrumpController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class TrumpController : MonoBehaviour
+{
+    private Animation anim;
+    private Rigidbody rb;
+    [SerializeField] private float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+        rb.velocity = movement * speed;
+
+        // куда объект движется, туда он и смотрит
+        if(x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x,y)*Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+
+        if(x !=0 || y != 0)
+        {
+            anim.Play("Walk");
+        }
+        else
+        {
+            anim.Play("Idle");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VuforiaProject
 Augment reality Unity project
+Unity version 2019.4.22f1
+Для сборки: Assets\Scenes\Denomination
+
+При наведении камеры на купюру 50 рублей появляется персонаж которым можно управлять джойстиком.
